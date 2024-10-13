3b2390ec47f3f4d678fa07e0d0804535f04902d2
NguyenThanhhToan <toanthanhnguyen2302@gmail.com>
2024-10-13 14:37:09 +0700

    fix :

--- src/main/java/com/project/ShopKoi/repository/UserRepository.java
+++ src/main/java/com/project/ShopKoi/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.project.ShopKoi.repository;
 
 import com.project.ShopKoi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
    Optional<User> findByEmail(String email);
 
