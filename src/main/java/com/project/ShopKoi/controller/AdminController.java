3dd1847e079b964961df740b3670d38a9c810c51
NguyenThanhhToan <toanthanhnguyen2302@gmail.com>
2024-10-20 13:38:58 +0700

    feat :add AdminController

---
+++ src/main/java/com/project/ShopKoi/controller/AdminController.java
@@ -0,0 +1,47 @@
+package com.project.ShopKoi.controller;
+
+import com.project.ShopKoi.model.dto.UserDto;
+import com.project.ShopKoi.model.enums.OrderStatus;
+import com.project.ShopKoi.service.impl.AdminServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+    private final AdminServiceImpl adminServiceImpl;
+
+
+    @GetMapping("users")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(adminServiceImpl.getAllUsers());
+    }
+
+    @GetMapping("/users/{roleName}")
+    public ResponseEntity<List<UserDto>> getUsersByRole(@PathVariable String roleName) {
+        List<UserDto> users = adminServiceImpl.getAllUsersByRole(roleName);
+        return ResponseEntity.ok(users);
+    }
+    @PutMapping("/change-role/{userId}")
+    public ResponseEntity<String> updateUserRole(@PathVariable Long userId) {
+        String response = adminServiceImpl.changeUserRole(userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/user-statistics")
+    public ResponseEntity<Map<String, Long>> getUserStatistics() {
+        Map<String, Long> statistics = adminServiceImpl.getUserStatistics();
+        return ResponseEntity.ok(statistics);
+    }
+
+    @GetMapping("/order-statistics")
+    public ResponseEntity<Map<OrderStatus, Long>> getOrderStatistics() {
+        Map<OrderStatus, Long> statistics = adminServiceImpl.getOrderStatistics();
+        return ResponseEntity.ok(statistics);
+    }
+
+}
