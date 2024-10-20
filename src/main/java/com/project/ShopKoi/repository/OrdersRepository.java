67fc81ea54d7a19ea9044426feae1701007350b3
NguyenThanhhToan <toanthanhnguyen2302@gmail.com>
2024-10-20 13:51:25 +0700

    feat: add jpa find orders

--- src/main/java/com/project/ShopKoi/repository/OrdersRepository.java
+++ src/main/java/com/project/ShopKoi/repository/OrdersRepository.java
@@ -1,7 +1,11 @@
 package com.project.ShopKoi.repository;
 
 import com.project.ShopKoi.model.entity.Orders;
+import com.project.ShopKoi.model.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    @Query("SELECT COUNT(o) FROM Orders o WHERE o.status = ?1")
+    long countByStatus(OrderStatus status);
 }
